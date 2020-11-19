197392ae59cd5cbde6bb7d9ed65118ef419c7855
PatriciaArnedo <paaarnedo@gmail.com>
2020-11-19 16:36:31 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+
+        },
+        body: JSON.stringify({
+            //pass in obj key arguments
+            name,
+            email
+        }),
+    })
+    .then(response => response.json())
+    .then(object => {
+        let id = object["id"]
+        const body = document.querySelector("body")
+        const idDiv = document.createElement("div")
+        idDiv.textContent = id
+        body.appendChild(idDiv)
+    })
+    .catch(error => {
+        const body = document.querySelector("body")
+        const errorDiv = document.createElement("div")
+        errorDiv.textContent = error.message
+        body.appendChild(errorDiv)
+    })
+}
